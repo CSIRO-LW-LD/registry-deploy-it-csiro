c5a0e3c1c14a56bc3861678cf4cf6eecb7841c5a
Ben Leighton <Ben.Leighton@csiro.au>
2014-05-14 11:00:29 +1000

    Uses registry-core maven build instead of a prebuilt release Added new default admins

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ RELEASE=snapshot/com/github/ukgovld/registry-core/0.0.2-SNAPSHOT/registry-core-0
 
 apt-get update -y
 apt-get install -y curl chkconfig
+apt-get install -y git 
+apt-get install -y maven
 
 # install and configure tomcat
 echo "** Installing java and tomcat"
@@ -95,7 +97,17 @@ chkconfig nginx on
 # Set up configuration area /opt/ldregistry
 echo "** Installing registry application"
 rm -rf /var/lib/tomcat7/webapps/ROOT*
-curl -4s https://s3-eu-west-1.amazonaws.com/ukgovld/$RELEASE > /var/lib/tomcat7/webapps/ROOT.war
+#curl -4s https://s3-eu-west-1.amazonaws.com/ukgovld/$RELEASE > /var/lib/tomcat7/webapps/ROOT.war
+sudo su
+service tomcat7 stop
+rm -rf /var/opt/ldregistry/*
+cd ~/
+git clone https://github.com/UKGovLD/registry-core.git
+cd registry-core
+mvn clean package
+cd target
+cp registry*.war /var/lib/tomcat7/webapps/ROOT.war
+service tomcat7 start
 
 if [ $(grep -c -e 'tomcat.*/opt/ldregistry/proxy-conf.sh' /etc/sudoers) -ne 0 ]
 then
